9a3442fdc04453a438e6465d3ad90ed04df7264b
Marcus-Kanon <94marsun@gafe.molndal.se>
2021-09-07 10:58:29 +0200

    Skapade projekt!

---
+++ Inlämningsuppgift 1/Inlämningsuppgift/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inlämningsuppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dinero=500; //Pengar man startar med
+            int antalTarningar = 3;
+
+            Random rnd = new Random();
+            int[] slumpTal = new int[antalTarningar];
+
+
+
+            Console.WriteLine("Välkommer till spelautomaten där du spelar bort dina hårt tjänade pengar! \n Du har {0} kr", dinero);
+
+
+        }
+    }
+}
