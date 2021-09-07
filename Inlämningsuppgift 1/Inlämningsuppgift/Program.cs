a11d9aeb9f978aab4f9cdaf317f1013d20fbed3f
Marcus-Kanon <94marsun@gafe.molndal.se>
2021-09-07 11:04:23 +0200

    Gjorde en while loop

--- Inlämningsuppgift 1/Inlämningsuppgift/Program.cs
+++ Inlämningsuppgift 1/Inlämningsuppgift/Program.cs
@@ -8,13 +8,22 @@ namespace Inlämningsuppgift
         {
             int dinero=500; //Pengar man startar med
             int antalTarningar = 3;
+            int satsning;
+            int tal;
 
             Random rnd = new Random();
             int[] slumpTal = new int[antalTarningar];
 
-
-
             Console.WriteLine("Välkommer till spelautomaten där du spelar bort dina hårt tjänade pengar! \n Du har {0} kr", dinero);
+
+            while(dinero>=50)
+            {
+                Console.WriteLine("Hur mycket vill du satsa?");
+                satsning=int.Parse(Console.ReadLine());
+                tal=int.Parse(Console.ReadLine());
+
+
+            }
 
 
         }
