4befb1c1a1f662b0eaebd757f4d564776d9f0132
Marcus-Kanon <94marsun@gafe.molndal.se>
2021-09-07 11:21:57 +0200

    Första testet

--- Inlämningsuppgift 1/Inlämningsuppgift/Program.cs
+++ Inlämningsuppgift 1/Inlämningsuppgift/Program.cs
@@ -6,23 +6,46 @@ namespace Inlämningsuppgift
     {
         static void Main(string[] args)
         {
-            int dinero=500; //Pengar man startar med
+            int saldo=500; //Pengar man startar med
             int antalTarningar = 3;
             int satsning;
             int tal;
-
             Random rnd = new Random();
             int[] slumpTal = new int[antalTarningar];
+            int antalLika=0;
 
-            Console.WriteLine("Välkommer till spelautomaten där du spelar bort dina hårt tjänade pengar! \n Du har {0} kr", dinero);
+            Console.WriteLine("Välkommer till spelautomaten där du spelar bort dina hårt intjänade pengar! \n\nDu har {0} kr\n", saldo);
 
-            while(dinero>=50)
+            while (saldo >= 50)
             {
                 Console.WriteLine("Hur mycket vill du satsa?");
-                satsning=int.Parse(Console.ReadLine());
-                tal=int.Parse(Console.ReadLine());
+                satsning = int.Parse(Console.ReadLine());
+                tal = int.Parse(Console.ReadLine());
 
+                for (int i = 0; i < antalTarningar; ++i)
+                {
+                    slumpTal[i] = rnd.Next(1, 7);
+                    Console.WriteLine("Tärning #{0}: {1}", i + 1, slumpTal[i]);
 
+                    if (slumpTal[i] == tal)
+                    {
+                        antalLika++;
+                    }
+                }
+
+                if (antalLika > 0)
+                { 
+                    Console.WriteLine("{0} rätt! Du vinner {1}", antalLika + 1, (antalLika + 1) * satsning); //(antalLika+1)*satsning imvandlar antal rätt till vinst i pengar
+                    saldo += (antalLika + 1) * satsning;
+                }
+                else
+                {
+                    Console.WriteLine("Du gissade fel!");
+                }
+
+                Console.WriteLine("Saldo: {0}", saldo);
+
+                antalLika = 0;
             }
 
 
