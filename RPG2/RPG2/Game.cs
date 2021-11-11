2f0b6c6d68ee1049b58824eda4fffbab5fc7ff0a
Marcus-Kanon <94marsun@gafe.molndal.se>
2021-11-11 15:31:22 +0100

    Legendarisk kod för menyn klar

--- RPG2/RPG2/Game.cs
+++ RPG2/RPG2/Game.cs
@@ -8,24 +8,7 @@ namespace RPG2
 {
     public static class Game
     {
-        static int choice = 0;
-        public static int ChoiceLimit = 0;
-        public static int Choice
-        {
-            get
-            {
-                return choice;
-            }
-            set
-            {
-                if (value >= ChoiceLimit)
-                    value = 0;
-                if (value < 0)
-                    value = ChoiceLimit-1;
-                choice = value;
-            }
-        }
-        static int oldChoice = 0;
+        
 
         static Game()
         {
@@ -38,31 +21,16 @@ namespace RPG2
 
         public static void Start()
         {
-            ChoiceLimit = 3;
-            Scenes.MenuMain(Choice);
-
             InputHandler.Start();
-            while(true)
-            {
-                if(choice!=oldChoice)
-                {
-                    //Scenes.Update = true;
-                    Scenes.MenuMain(Choice);
-                    oldChoice = Choice;
-                }
-                    
-            }
-
+            Scenes.MenuStarter(Scenes.MenuMain, 3);
 
         }
 
         public static void OnUpKey(object? obj, EventArgs args)
         {
-            Choice--;
         }
         public static void OnDownKey(object? obj, EventArgs args)
         {
-            Choice++;
         }
         public static void OnLeftKey(object? obj, EventArgs args)
         {
--- RPG2/RPG2/Scenes.cs
+++ RPG2/RPG2/Scenes.cs
@@ -8,35 +8,90 @@ namespace RPG2
 {
     public static class Scenes
     {
-        public static bool Update = false;
-
-        public static void MenuMain(int frame)
+        static int choice = 0;
+        public static int ChoiceLimit = 0;
+        static int OldChoice = 0;
+        public static int Choice
         {
+            get
+            {
+                return choice;
+            }
+            set
+            {
+                if (value >= ChoiceLimit)
+                    value = 0;
+                if (value < 0)
+                    value = ChoiceLimit - 1;
+                choice = value;
+            }
+        }
+
+
+        static Scenes()
+        {
+            InputHandler.OnUpKeyHandler += OnUpKey;
+            InputHandler.OnDownKeyHandler += OnDownKey;
+            InputHandler.OnLeftKeyHandler += OnLeftKey;
+            InputHandler.OnRightKeyHandler += OnRightKey;
+
+        }
+
+        public static void MenuStarter(Action method, int limit)
+        {
+            ChoiceLimit = limit;
+            method();
+
+            while (true)
+            {
+                if (choice != OldChoice)
+                {
+                    method();
+                    OldChoice = Choice;
+                }
+
+            }
+        }
+
+        public static void MenuMain()
+        {
+            
             Console.Clear();
             ConsoleColor color = ConsoleColor.White;
-            //if(Update)
-            //{
-                if (frame == 0)
-                {
-                    "> Start Game <\n".Print(color, 0, 0);
-                    "Highscore (... Like you have any)\n".Print(color, 0, 0);
-                    "End Game\n".Print(color, 0, 0);
-                }
-                else if (frame == 1)
-                {
-                    "Start Game\n".Print(color, 0, 0);
-                    "> Highscores (... Like you have any) <\n".Print(color, 0, 0);
-                    "End Game\n".Print(color, 0, 0);
-                }
-                else if (frame == 2)
-                {
-                    "Start Game\n".Print(color, 0, 0);
-                    "Highscore (... Like you have any)\n".Print(color, 0, 0);
-                    "> End Game <\n".Print(color, 0, 0);
-                }
-            //} 
 
-            Update = false;
+                if (Choice == 0)
+                {
+                    "> Start Game <\n".Print(ConsoleColor.DarkGreen, 0, 0);
+                    "Highscores (... Like you have any)\n".Print(color, 0, 0);
+                    "End Game\n".Print(color, 0, 0);
+                }
+                else if (Choice == 1)
+                {
+                    "Start Game\n".Print(color, 0, 0);
+                    "> Highscores (... Like you have any) <\n".Print(ConsoleColor.DarkGreen, 0, 0);
+                    "End Game\n".Print(color, 0, 0);
+                }
+                else if (Choice == 2)
+                {
+                    "Start Game\n".Print(color, 0, 0);
+                    "Highscores (... Like you have any)\n".Print(color, 0, 0);
+                    "> End Game <\n".Print(ConsoleColor.DarkGreen, 0, 0);
+                }
+        }
+
+        public static void OnUpKey(object? obj, EventArgs args)
+        {
+            Scenes.Choice--;
+        }
+        public static void OnDownKey(object? obj, EventArgs args)
+        {
+            Scenes.Choice++;
+        }
+        public static void OnLeftKey(object? obj, EventArgs args)
+        {
+        }
+        public static void OnRightKey(object? obj, EventArgs args)
+        {
         }
 
     }
