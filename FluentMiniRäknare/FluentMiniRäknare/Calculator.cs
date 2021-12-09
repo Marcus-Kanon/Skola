efb90a3597c98e4ae0008d2fa82694f4ffdb3766
Marcus-Kanon <94marsun@gafe.molndal.se>
2021-12-09 11:28:55 +0100

    Done

---
+++ FluentMiniRäknare/FluentMiniRäknare/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentMiniRäknare
+{
+    public class Calculator
+    {
+        List<double> mem = new List<double>();
+        char op;
+        public Calculator Digit(double d)
+        {
+            mem.Add(d);
+            return this;
+        }
+        public Calculator Operator(char ope)
+        {
+            op = ope;
+            return this;
+        }
+        public Calculator Clear(char ope)
+        {
+            mem.Clear();
+            return this;
+        }
+        public double Result()
+        {
+            switch (op)
+            {
+                case '+':
+                    return mem.Sum();
+                default:
+                     return mem.Sum();
+            }
+        }
+
+    }
+}
---
+++ FluentMiniRäknare/FluentMiniRäknare/Program.cs
@@ -0,0 +1,7 @@
+﻿using FluentMiniRäknare;
+
+var calculator = new Calculator();
+
+var digit = calculator.Digit(5.5).Digit(1.1).Operator('+').Result();
+
+Console.WriteLine(digit);
