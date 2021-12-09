864a130ed355974a74037773e15748a6d08f0fed
Marcus-Kanon <94marsun@gafe.molndal.se>
2021-12-09 11:43:52 +0100

    Mer avancerad

--- FluentMiniRäknare/FluentMiniRäknare/Calculator.cs
+++ FluentMiniRäknare/FluentMiniRäknare/Calculator.cs
@@ -8,33 +8,49 @@ namespace FluentMiniRäknare
 {
     public class Calculator
     {
-        List<double> mem = new List<double>();
-        char op;
+        List<double> _mem = new List<double>();
+        List<char> _op = new List<char>();
+
+        public Calculator()
+        {
+            _op.Add('+');
+        }
         public Calculator Digit(double d)
         {
-            mem.Add(d);
+            _mem.Add(d);
             return this;
         }
-        public Calculator Operator(char ope)
+        public Calculator Operator(char op)
         {
-            op = ope;
+            _op.Add(op);
             return this;
         }
         public Calculator Clear(char ope)
         {
-            mem.Clear();
+            _mem.Clear();
+            _op.Clear();
+            _op.Add('+');
             return this;
         }
         public double Result()
         {
-            switch (op)
+            double result = 0;
+            for (int i = 0; i < _mem.Count; i++)
             {
-                case '+':
-                    return mem.Sum();
-                default:
-                     return mem.Sum();
+                switch (_op[i])
+                {
+                    case '+':
+                        result += _mem[i];
+                        break;
+                    case '-':
+                        result -= _mem[i];
+                        break;
+                    default:
+                        return _mem.Sum();
+                }
             }
-        }
 
+            return result;
+        }
     }
 }
--- FluentMiniRäknare/FluentMiniRäknare/Program.cs
+++ FluentMiniRäknare/FluentMiniRäknare/Program.cs
@@ -2,6 +2,6 @@
 
 var calculator = new Calculator();
 
-var digit = calculator.Digit(5.5).Digit(1.1).Operator('+').Result();
+var digit = calculator.Digit(5.5).Operator('+').Digit(1.1).Operator('-').Digit(6.5).Result();
 
 Console.WriteLine(digit);
