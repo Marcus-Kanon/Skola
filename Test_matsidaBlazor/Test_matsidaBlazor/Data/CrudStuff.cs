06d8a53be0d9331b1e431f3df5468d6c8d59e001
Marcus-Kanon <94marsun@gafe.molndal.se>
2021-12-26 19:48:09 +0100

    Fungerar att söka och kolla detaljer nu. Crud flyttad till egen klass

---
+++ Test_matsidaBlazor/Test_matsidaBlazor/Data/CrudStuff.cs
@@ -0,0 +1,51 @@
+﻿using Test_matsidaBlazor.Data.Models;
+
+namespace Test_matsidaBlazor.Data
+{
+    public static class CrudStuff
+    {
+        static DataContext _context = new();
+        static List<Recipe> _recipes = new();
+
+        public static Recipe GetRecipe(int id)
+        {
+
+            Recipe? recipe = null;
+
+            if (_context != null)
+            {
+                recipe = _context.Recipes.FirstOrDefault(q => q.Id == id);
+            }
+
+            if (recipe != null)
+            {
+                return recipe;
+            }
+
+
+            return new Recipe();
+        }
+
+        public static List<Recipe> SearchRecipes(string search)
+        {
+            List<Recipe> results = new();
+
+            if (search != "")
+            {
+                results = _recipes.FindAll(q => q.Name.Contains(search, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return results;
+        }
+
+        public static void LoadRecipes()
+        {
+            _recipes = _context.Recipes.ToList();
+        }
+
+        public static void LoadContext(DataContext context)
+        {
+            _context = context;
+        }
+    }
+}
