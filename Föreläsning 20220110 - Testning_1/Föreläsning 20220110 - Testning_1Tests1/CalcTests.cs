0af595834d68d309550602f5081096b0094cce55
Marcus-Kanon <94marsun@gafe.molndal.se>
2022-01-11 13:09:47 +0100

    IngredientSelector updateringar

---
+++ Föreläsning 20220110 - Testning_1/Föreläsning 20220110 - Testning_1Tests1/CalcTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using calculator;
+
+namespace Tests
+{
+    [TestClass()]
+    public class CalcTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            //Arrange
+            //var testObject = Calc;
+            var expected = 8;
+
+            //Act
+            var actual = Calc.Add(4, 4);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AddTest_Sum16()
+        {
+            //Arrange
+            //var testObject = new Calc();
+            var expected = 16;
+
+            //Act
+            var actual = Calc.Add(8, 8);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass()]
+    public class PhoneTests
+    {
+        [TestMethod()]
+        public void VerifyPhoneNumberTest()
+        {
+            //Arrange
+            var testObject = new VerifyStuff();
+            var expected = true;
+            var testData = "+";
+
+            //Act
+            var actual = testObject.VerifyPhoneNumber(testData);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
